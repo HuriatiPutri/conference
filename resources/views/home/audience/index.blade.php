b1e9756f4d7efac9be6b3f195eb31212309233b9
Putri Huriati <huriatiputri@gmail.com>
2025-09-07 22:42:43 +0700

    feat: add download data table

--- resources/views/home/audience/index.blade.php
+++ resources/views/home/audience/index.blade.php
@@ -1,5 +1,5 @@
 @php
-use App\Constants\Countries;
+  use App\Constants\Countries;
 @endphp
 @extends('adminlte::page')
 
@@ -30,43 +30,43 @@ use App\Constants\Countries;
               <div class="card-body">
                 <h5>Filter Data Audience</h5>
                 <div class="row">
-                <div class="col-md-4">
-                  <div class="mb-3 flex gap-4">
-                    <select id="filterConference" class="form-control">
-                      <option value="">-- Semua Conference --</option>
-                      @foreach($conferences as $conference)
-                        <option value="{{ $conference->name }}">{{ $conference->name }}</option>
-                      @endforeach
+                  <div class="col-md-4">
+                    <div class="mb-3 flex gap-4">
+                      <select id="filterConference" class="form-control">
+                        <option value="">-- Semua Conference --</option>
+                        @foreach ($conferences as $conference)
+                          <option value="{{ $conference->name }}">{{ $conference->name }}</option>
+                        @endforeach
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-md-4 mb-3">
+                    <select id="filterPaymentMethod" class="form-control">
+                      <option value="">-- Semua Metode Pembayaran --</option>
+                      <option value="Bank Transfer">Bank Transfer</option>
+                      <option value="Payment Gateway">Payment Gateway</option>
+                      <!-- tambah sesuai kebutuhan -->
                     </select>
-                  </div>                  
-                </div>
-                <div class="mb-3 col-md-4">
-                  <select id="filterPaymentMethod" class="form-control">
-                    <option value="">-- Semua Metode Pembayaran --</option>
-                    <option value="Bank Transfer">Bank Transfer</option>
-                    <option value="Payment Gateway">Payment Gateway</option>
-                    <!-- tambah sesuai kebutuhan -->
-                  </select>
-                </div>
-                <div class="mb-3 col-md-4">
-                  <select id="filterPaymentStatus" class="form-control">
-                    <option value="">-- Semua Status Pembayaran --</option>
-                    <option value="Paid">Paid</option>
-                    <option value="Pending Payment">Payment Pending</option>
-                    <option value="Cancelled">Cancelled</option>
-                    <option value="Refunded">Refunded</option>
-                    <!-- tambah sesuai kebutuhan -->
-                  </select>
-                </div>
+                  </div>
+                  <div class="col-md-4 mb-3">
+                    <select id="filterPaymentStatus" class="form-control">
+                      <option value="">-- Semua Status Pembayaran --</option>
+                      <option value="Paid">Paid</option>
+                      <option value="Pending Payment">Payment Pending</option>
+                      <option value="Cancelled">Cancelled</option>
+                      <option value="Refunded">Refunded</option>
+                      <!-- tambah sesuai kebutuhan -->
+                    </select>
+                  </div>
                 </div>
 
                 <div class="mb-3 flex flex-col gap-6 sm:flex-row">
-                  <span id="summaryPaid" class="bg-green p-2 rounded">Paid 0</span>
-                  <span id="summaryPending" class="bg-yellow p-2 rounded">Pending Payment 0</span>
-                  <span id="summaryExpired" class="bg-red p-2 rounded">Cancelled/Refunded 0</span>
-                  <span id="summaryRefunded" class="bg-gray p-2 rounded">Refunded 0</span>
+                  <span id="summaryPaid" class="bg-green rounded p-2">Paid 0</span>
+                  <span id="summaryPending" class="bg-yellow rounded p-2">Pending Payment 0</span>
+                  <span id="summaryExpired" class="bg-red rounded p-2">Cancelled/Refunded 0</span>
+                  <span id="summaryRefunded" class="bg-gray rounded p-2">Refunded 0</span>
                 </div>
-                <hr/>
+                <hr />
                 <div class="table-responsive">
                   <table id="audiencesTable" class="table-bordered table-hover table-striped table">
                     <thead>
@@ -88,26 +88,34 @@ use App\Constants\Countries;
                     </thead>
                     <tbody>
                       @forelse($audiences as $audience)
-                      @php
-                      if($audience->phone_number) {
-                        $clearPhoneNumber = preg_replace('/[\s\-\(\)\+]/', '', $audience->phone_number);
-                        if(preg_match('/^0/', $clearPhoneNumber) && isset(Countries::LIST[$audience->country])) {
-                          $phoneNumber = preg_replace('/^0/', Countries::LIST[$audience->country]['code'], $clearPhoneNumber);
-                        } else if (preg_match('/^(?!\+|0)/', $clearPhoneNumber)) {
-                          $phoneNumber = $clearPhoneNumber;
-                        } else {
-                          $phoneNumber = (isset(Countries::LIST[$audience->country]) ? Countries::LIST[$audience->country]['code'] : '') . $clearPhoneNumber;
-                        }
-                        $audience->phone_number = $phoneNumber;
-                      }
-                      @endphp
+                        @php
+                          if ($audience->phone_number) {
+                              $clearPhoneNumber = preg_replace('/[\s\-\(\)\+]/', '', $audience->phone_number);
+                              if (preg_match('/^0/', $clearPhoneNumber) && isset(Countries::LIST[$audience->country])) {
+                                  $phoneNumber = preg_replace(
+                                      '/^0/',
+                                      Countries::LIST[$audience->country]['code'],
+                                      $clearPhoneNumber,
+                                  );
+                              } elseif (preg_match('/^(?!\+|0)/', $clearPhoneNumber)) {
+                                  $phoneNumber = $clearPhoneNumber;
+                              } else {
+                                  $phoneNumber =
+                                      (isset(Countries::LIST[$audience->country])
+                                          ? Countries::LIST[$audience->country]['code']
+                                          : '') . $clearPhoneNumber;
+                              }
+                              $audience->phone_number = $phoneNumber;
+                          }
+                        @endphp
                         <tr>
                           <td>{{ $audience->id }}</td>
                           <td>{{ $audience->conference->name ?? 'N/A' }}</td>
                           <td>{{ $audience->first_name }}</td>
                           <td>{{ $audience->last_name }}</td>
                           <td>{{ $audience->email }}</td>
-                          <td><a href="https://wa.me/{{$audience->phone_number}}" target="_blank"> {{$audience->phone_number }}</a></td>
+                          <td><a href="https://wa.me/{{ $audience->phone_number }}" target="_blank">
+                              {{ $audience->phone_number }}</a></td>
                           <td>{{ Str::headline($audience->presentation_type) }}</td>
                           <td>
                             {{ $audience->getPaymentMethodText() }}
@@ -234,12 +242,54 @@ use App\Constants\Countries;
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.min.js"></script>
 
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+
+
+    <!-- DataTables Buttons -->
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+
+    <!-- Library untuk Excel -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
     <script>
       function regexEscape(text) {
         return text.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
       }
       $(document).ready(function() {
         var table = $('#audiencesTable').DataTable({
+          "dom": 'Bfrtip',
+          "buttons": [
+            {
+              extend: 'excelHtml5',
+              text: '<i class="fas fa-file-excel"></i> Export to Excel',
+              className: 'btn btn-info mb-3',
+              title: 'Daftar Audience',
+              exportOptions: {
+                columns: ':not(:last-child)' // Exclude the last column (Aksi)
+              }
+            },
+            {
+              extend: 'csvHtml5',
+              text: '<i class="fas fa-file-csv"></i> Export to CSV',
+              className: 'btn btn-info mb-3',
+              title: 'Daftar Audience',
+              exportOptions: {
+                columns: ':not(:last-child)' // Exclude the last column (Aksi)
+              }
+            },
+            {
+              extend: 'print',
+              text: '<i class="fas fa-print"></i> Print',
+              className: 'btn btn-info mb-3',
+              title: 'Daftar Audience',
+              exportOptions: {
+                columns: ':not(:last-child)' // Exclude the last column (Aksi)
+              }
+            }
+          ],
           "language": {
             "url": "//cdn.datatables.net/plug-ins/2.0.8/i18n/id.json"
           },
@@ -280,7 +330,9 @@ use App\Constants\Countries;
         });
 
         function updateSummary() {
-          var data = table.rows({ filter: 'applied' }).data();
+          var data = table.rows({
+            filter: 'applied'
+          }).data();
           var transferCount = 0;
           var gatewayCount = 0;
           var cancelledCount = 0;
@@ -288,23 +340,23 @@ use App\Constants\Countries;
 
           data.each(function(row) {
             // asumsi kolom payment_method di index 12
-            var payment = row[9]; 
+            var payment = row[9];
             if (payment.toLowerCase().includes('paid')) {
               transferCount++;
             } else if (payment.toLowerCase().includes('pending payment')) {
               gatewayCount++;
-            }else if (payment.toLowerCase().includes('cancelled')) {
+            } else if (payment.toLowerCase().includes('cancelled')) {
               cancelledCount++;
-            }else if (payment.toLowerCase().includes('refunded')) {
+            } else if (payment.toLowerCase().includes('refunded')) {
               refundedCount++;
             }
           });
 
           $('#summaryPaid').text('Paid: ' + transferCount);
           $('#summaryPending').text('Pending: ' + gatewayCount);
-          $('#summaryCancelled').text('Cancelled/Refunded: ' + cancelledCount); 
-          $('#summaryRefunded').text('Refunded: ' + refundedCount); 
-        }        
+          $('#summaryCancelled').text('Cancelled/Refunded: ' + cancelledCount);
+          $('#summaryRefunded').text('Refunded: ' + refundedCount);
+        }
       });
     </script>
 
