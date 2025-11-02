9dfb8a848dd916f4177e75aa65087973389aa076
PUTRI HURIATI <putri.huriati@digitaltelkom.id>
2025-11-03 01:49:07 +0700

    fix: paypal env

--- config/paypal.php
+++ config/paypal.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'client_id' => env('PAYPAL_SANDBOX_CLIENT_ID'),
-    'client_secret' => env('PAYPAL_SANDBOX_CLIENT_SECRET'),
+    'client_id' => env('PAYPAL_LIVE_CLIENT_ID'),
+    'client_secret' => env('PAYPAL_LIVE_CLIENT_SECRET'),
     'mode' => env('PAYPAL_MODE', 'sandbox'), // sandbox or live
     'sandbox_url' => 'https://api.sandbox.paypal.com',
     'live_url' => 'https://api.paypal.com',
